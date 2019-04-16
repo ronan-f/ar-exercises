403cbd228d31d58d2ee2557557ae26f3184208cc
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 21:11:51 +0000

    Created relationship between store and employees and created multiple employees

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,8 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-@store1 = Store.where(id: 1).load # reference to the first store
-@store2 = Store.where(id: 2).load
+@store1 = Store.find(1) # reference to the first store
+@store2 = Store.find(2)
 
 puts "Changing store name..."
 @store1.update(:name => 'NewName')
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ronan", last_name: "Fegan", hourly_rate: 20)
+@store1.employees.create(first_name: "Aidan", last_name: "Employee", hourly_rate: 22)
+
+@store2.employees.create(first_name: "Aoibheann", last_name: "Employee", hourly_rate: 65)
+@store2.employees.create(first_name: "Helena", last_name: "Employee", hourly_rate: 24)
+@store2.employees.create(first_name: "Kieran", last_name: "Employee", hourly_rate: 90)
