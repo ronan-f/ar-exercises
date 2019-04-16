0bfdb6d0245694289ed4f79d81fdd13032c16704
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 20:21:01 +0000

    Ran queries to check total revenue/avg revenue/number of stores with X revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum('annual_revenue')
+
+puts "Total revenue for all stores: #{@total_revenue}"
+
+@average_revenue = @total_revenue / Store.count
+
+puts "Average revenue per store is: #{@average_revenue}"
+
+@rich_stores = Store.where("annual_revenue >= 1000000").count
+
+puts "Number of stores with a revenue >= $1M: #{@rich_stores}"
