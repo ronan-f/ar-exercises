d6eb82774f4c36f451d3fb467c9eb0ef4312a75e
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 19:27:08 +0000

    Updated store names

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,15 @@ puts "----------"
 # Your code goes below here ...
 
 Store.create(:name => 'Burnaby', :annual_revenue => 300000, :mens_apparel => true, :womens_apparel => true)
-Store.create(:name => 'Richmond', :annual_revenue => 1260000, :mens_apparel => true, :womens_apparel => true)
-Store.create(:name => 'Gastown', :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => true)
+Store.create(:name => 'Richmond', :annual_revenue => 1260000, :mens_apparel => false, :womens_apparel => true)
+Store.create(:name => 'Gastown', :annual_revenue => 190000, :mens_apparel => true, :womens_apparel => false)
 
 puts "The number of stores is: #{Store.count}"
+
+@store1 = Store.where(id: 1).load # reference to the first store
+@store2 = Store.where(id: 2).load
+
+puts "Changing store name..."
+@store1.update(:name => 'NewName')
+
+
