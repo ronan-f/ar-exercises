faf66626bff60b657f21536e8fc5a9d6e6d52ec8
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 19:38:11 +0000

    Deleted store 3 using destroy

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -4,5 +4,12 @@ require_relative './exercise_2'
 
 puts "Exercise 3"
 puts "----------"
+puts "Deleting store 3..."
 
 # Your code goes here ...
+
+@store3 = Store.where(id: 3).load
+
+@store3.destroy_all
+
+puts "The number of stores is: #{Store.count}"
