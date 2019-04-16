4ac13666e7106b42d93061738f2c30e9d6e8d9f8
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 23:08:20 +0000

    Added validation with error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts 'Choose a name for your new store: '
+
+new_store_name = gets.chomp
+
+@new_store = Store.create(:name => new_store_name)
+
+if @new_store.errors.any?
+    @new_store.errors.each do |attribute, message|
+        puts "ERROR: #{attribute} #{message}"
+    end
+end
+
+
+
+
+
+
+
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
     belongs_to :store
+    validates_associated :store
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :hourly_rate, presence: true
+    validates :hourly_rate, :numericality => { :greater_than => 40, :less_than => 200 }
 end
