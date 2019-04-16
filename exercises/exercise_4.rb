c1e3ec17dd5eef72a1687be966a651d96f78a56f
Ronan Fegan <rfegan13@gmail.com>
2019-04-16 20:14:03 +0000

    Ran queries returning stores with X revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+
+Store.create(:name => 'Surrey', :annual_revenue => 224000, :mens_apparel => false, :womens_apparel => true)
+Store.create(:name => 'Whistler', :annual_revenue => 1900000, :mens_apparel => true, :womens_apparel => false)
+Store.create(:name => 'Yaletown', :annual_revenue => 430000, :mens_apparel => true, :womens_apparel => true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts 'Mens stores with associated annual revenue: '
+
+@mens_stores.each do |store|
+    puts "Store Name: #{store.name}"
+    puts "Store revenue: #{store.annual_revenue}"
+end
+
+@less_than_million_women = Store.where('womens_apparel = true AND annual_revenue < 1000000')
+
+puts 'Womens stores with less than $1M in annual revenue: '
+
+@less_than_million_women.each do |store|
+    puts "Store Name: #{store.name}"
+    puts "Store revenue: #{store.annual_revenue}"
+end
+
